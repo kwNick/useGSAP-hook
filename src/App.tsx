e5afb7206e0920f98257f8bd54aad994043367b3
Nickolas Piraino <kwnp17c@gmail.com>
2024-05-07 11:33:53 -0400

    fixed the gsap problem and added the css from the docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import React from 'react';
 
+const { useRef } = React;
+// console.log(React.version);
 function App() {
-  const [count, setCount] = useState(0)
+  const container: any = useRef();
+  const circle: any = useRef();
+
+  useGSAP(() => {
+    // use selectors...
+    gsap.to(".box", { rotation: "+=360", duration: 3 });
+
+    // or refs...
+    gsap.to('.circle', { rotation: "-=360", duration: 3 });
+  },
+    { scope: container }
+  ); // <-- scope for selector text (optional)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div ref={container} className="container">
+        <div className="box gradient-blue">selector</div>
+        <div className="circle gradient-green" ref={circle}>
+          Ref
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="box gradient-blue">selector</div>
+    </div >
+  );
 }
 
 export default App

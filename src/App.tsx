60320c52e02ff9491e9d086525df84ae1fd0d4c2
Nickolas Piraino <kwnp17c@gmail.com>
2024-05-20 01:21:54 -0400

    removed unneccesary imports and files and cleaned up reviewed whatever

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import React from 'react';
